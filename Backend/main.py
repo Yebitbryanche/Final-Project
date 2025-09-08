a7726d9c4d0b26359aef81d6e0b51099131aff67
Yebitbryanche <yebitbryanche@gmail.com>
2025-09-08 12:19:44 +0100

    mboakako

--- Backend/main.py
+++ Backend/main.py
@@ -35,13 +35,6 @@ app.add_middleware(
 )
     
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
-    allow_credentials = True,
-    allow_methods = ["*"],
-    allow_headers = ["*"]
-)
 
 @app.on_event("startup")
 def on_startup():
