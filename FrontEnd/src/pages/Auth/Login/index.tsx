6cdf7012a7bd0c9bdd4452af5222fd605dd7b46a
Yebitbryanche <yebitbryanche@gmail.com>
2025-09-09 10:08:00 +0100

    registration

---
+++ FrontEnd/src/pages/Auth/Login/index.tsx
@@ -0,0 +1,166 @@
+import React, { useState } from 'react'
+import { FaLock, FaUser } from 'react-icons/fa';
+import { Link,useNavigate } from 'react-router-dom';
+import images from '../../../types/images';
+import axios from 'axios';
+import { base_URL } from '../signup/indeex';
+import { IoClose } from "react-icons/io5";
+
+function Login() {
+  const [user_name, setUser_name] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [message, setMessage] = useState("")
+  const [showmessage, setShowmessage] = useState(false)
+  const navigate = useNavigate()
+  const [isloading, setIsloading] = useState(false)
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    setIsloading(true)
+    try{
+       const response = await axios.post(`${base_URL}/login`,{user_name, password})
+
+       localStorage.setItem("token",response.data.access_token)
+        setUser_name("")
+        setPassword("")
+        console.log({ user_name });
+        setShowmessage(true)
+        setMessage("Login successfull ....") 
+        if(!user_name){
+            setMessage("User name required")
+        }
+        if(!password){
+            setMessage("Wrong password")
+        } 
+        setTimeout(()=>{
+            navigate("/dashboard")
+        },2000)
+      }
+    catch(err:any){
+       setMessage("failed to login please check credentials")
+       setShowmessage(true)
+    }
+    finally{
+      setIsloading(false)
+    }
+
+    
+  };
+
+  return (
+    <div className="flex justify-center items-center py-10 px-4">
+      {showmessage && (
+        <div
+          className={`absolute w-100 bottom-5 items-center rounded-sm text-white flex justify-between left-1 p-4 ${
+            message === "Login successfull ...." ? "bg-green-500" : "bg-red-500"
+          }`}
+        >
+          <p>{message}</p>
+          <IoClose
+            className="cursor-pointer"
+            size={30}
+            onClick={() => setShowmessage(false)}
+          />
+        </div>
+      )}
+      <div className="flex flex-col md:flex-row w-full max-w-5xl shadow-lg rounded-lg overflow-hidden">
+        {/* Left Side - Colored */}
+        <div
+          className="w-full md:w-1/2 flex flex-col justify-center items-center p-10 text-white 
+             md:rounded-tr-[100px] rounded-br-[100px]  "
+          style={{ backgroundColor: "var(--color-primary)" }}
+        >
+          <h1 className="text-4xl md:text-5xl font-bold mb-4 text-center">Shop Smart</h1>
+          <p className="text-lg md:text-xl text-center">
+            Login to your account for personalised deals
+          </p>
+        </div>
+
+        {/* Right Side - Form */}
+        <div className="w-full md:w-1/2 bg-white flex flex-col justify-center items-center p-6 md:p-10">
+          <h2 className="text-2xl md:text-3xl font-bold text-gray-800 mb-6">Login</h2>
+          <form onSubmit={handleSubmit} className="w-full max-w-sm" autoComplete="off">
+            {/* Email */}
+            <label className="block text-black-700 mb-1 font-medium">User Name</label>
+            <div className="flex items-center bg-secondary/10 rounded-lg mb-4 p-2 focus-within:ring-2 focus-within:ring-[var(--color-primary)]">
+              <FaUser className="text-black/50 mr-2" />
+              <input
+                type="text"
+                placeholder="Enter your User Name"
+                value={user_name}
+                onChange={(e) => setUser_name(e.target.value)}
+                autoComplete="off"
+                className="w-full p-2 outline-none"
+              />
+            </div>
+
+            {/* Password */}
+            <label className=" text-black-700 mb-1 font-medium">Password</label>
+            <div className="flex items-center bg-secondary/10 rounded-lg mb-2 p-2 focus-within:ring-2 focus-within:ring-[var(--color-primary)]">
+              <FaLock className="text-black/50 mr-2" />
+              <input
+                type={showPassword ? "text" : "password"}
+                placeholder="Enter your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                autoComplete="off"
+                className="w-full p-2 outline-none"
+              />
+            </div>
+
+            {/* Show password checkbox */}
+            <div className="flex items-center mb-4">
+              <input
+                type="checkbox"
+                id="showPassword"
+                checked={showPassword}
+                onChange={() => setShowPassword(!showPassword)}
+                className="mr-2"
+              />
+              <label htmlFor="showPassword" className="text-sm text-gray-600">
+                Show password
+              </label>
+            </div>
+
+           <div className='flex justify-between items-center'>
+              {/* Google login button */}
+              <button className="flex items-center justify-center gap-2 mb-2 py-2 px-6 rounded-lg hover:bg-gray-100 transition mx-auto">
+                <img src={images.google} alt="Google" className="w-6 h-6" />
+                <span className='text-sm'>Login with Google</span>
+              </button>
+              <button className="flex items-center justify-center gap-2 mb-2 py-2 px-6 rounded-lg hover:bg-gray-100 transition mx-auto">
+                <span className='text-sm'>Forgot password?</span>
+              </button>
+           </div>
+
+            {/* Login button */}
+            <button
+              type="submit"
+              className="px-10 cursor-pointer py-3 block mx-auto rounded-lg font-semibold text-white hover:opacity-80 transition mb-4"
+              style={{ backgroundColor: "var(--color-primary)" }}
+            >
+              Login
+            </button>
+          </form>
+
+          {/* Signup link */}
+          <p className="mt-4 text-sm text-gray-600 text-center">
+            Don&apos;t have an account?{" "}
+            <Link
+              to="/signup"
+              className="font-medium"
+              style={{ color: "var(--color-primary)" }}
+            >
+              Sign Up
+            </Link>
+          </p>
+        </div>
+      </div>
+    
+    </div>
+  );
+}
+
+export default Login
---
+++ FrontEnd/src/pages/Auth/signup/indeex.tsx
@@ -0,0 +1,209 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { FaEnvelope, FaLock, FaUser } from "react-icons/fa";
+import images from "../../../types/images";
+import { useNavigate } from "react-router-dom";
+export const base_URL = "http://127.0.0.1:8000";
+
+function Signup() {
+  const [user_name, setUser_name] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [role, setRole] = useState(false);
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate()
+
+  // Regex validators
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const usernameRegex = /^[a-zA-Z0-9._]{3,20}$/;
+  const passwordRegex =
+    /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+  const validateInputs = () => {
+    if (!user_name || !password || !email) {
+      setError("All fields are required.");
+      return false;
+    }
+    if (!usernameRegex.test(user_name)) {
+      setError(
+        "Username must be 3–20 characters long and can only contain letters, numbers, . and _"
+      );
+      return false;
+    }
+    if (!emailRegex.test(email)) {
+      setError("Invalid email format.");
+      return false;
+    }
+    if (!passwordRegex.test(password)) {
+      setError(
+        "Password must be at least 8 characters, include 1 uppercase letter, 1 number, and 1 special character."
+      );
+      return false;
+    }
+    return true;
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    setError("");
+    setSuccess("");
+
+    if (!validateInputs()) return; // block signup if invalid
+
+    try {
+      const response = await axios.post(`${base_URL}/signup`, {
+        user_name,
+        password,
+        email,
+        role,
+      });
+      console.log(response.data)
+
+      setSuccess("Account created successfully! 🎉");
+      setUser_name("");
+      setPassword("");
+      setEmail("");
+      setRole(false);
+        setTimeout(()=>{
+            navigate("/login")
+        },2000)
+    } catch (err: any) {
+      setError(err.response?.data?.detail || "Signup failed");
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center py-10 px-4">
+      {error && (
+        <div className="absolute w-100 top-1 md:bottom-1 items-center rounded-sm text-white flex justify-between bg-red-500 left-1 p-4">
+          <p className="text-white">{error}</p>
+        </div>
+      )}
+      {success && (
+        <div className="absolute w-100 top-1 md:bottom-1 items-center rounded-sm text-white flex justify-between bg-green-500 left-1 p-4">
+          <p className="text-white mb-2">{success}</p>
+        </div>
+      )}
+
+      <div className="flex flex-col md:flex-row w-full max-w-5xl shadow-lg rounded-lg overflow-hidden">
+        {/* Left Side */}
+        <div
+          className="w-full md:w-1/2 flex flex-col justify-center items-center p-10 text-white 
+             md:rounded-tr-[100px] rounded-br-[100px]"
+          style={{ backgroundColor: "var(--color-primary)" }}
+        >
+          <h1 className="text-4xl md:text-5xl font-bold mb-4 text-center">
+            Shop Smart
+          </h1>
+          <p className="text-lg md:text-2xl text-center">
+            Create your account for personalised deals
+          </p>
+        </div>
+
+        {/* Right Side */}
+        <div className="w-full md:w-1/2 bg-white flex flex-col justify-center items-center p-6 md:p-10">
+          <h2 className="text-2xl md:text-3xl font-bold text-gray-800 mb-6">
+            Sign Up
+          </h2>
+
+          <form onSubmit={handleSubmit} className="w-full max-w-sm">
+            {/* Username */}
+            <label className="block text-black-700 mb-1 font-medium">
+              Username
+            </label>
+            <div className="flex items-center bg-secondary/10 rounded-lg mb-4 p-2 focus-within:ring-2 focus-within:ring-[var(--color-primary)]">
+              <FaUser className="text-black/50 mr-2" />
+              <input
+                type="text"
+                required
+                placeholder="Enter your username"
+                value={user_name}
+                onChange={(e) => setUser_name(e.target.value)}
+                className="w-full p-2 outline-none focus:border-primary focus:ring-0"
+              />
+            </div>
+
+            {/* Email */}
+            <label className="block text-black-700 mb-1 font-medium">
+              Email
+            </label>
+            <div className="flex items-center bg-secondary/10 rounded-lg mb-4 p-2 focus-within:ring-2 focus-within:ring-[var(--color-primary)]">
+              <FaEnvelope className="text-black/50 mr-2" />
+              <input
+                required
+                type="email"
+                placeholder="Enter your email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="w-full p-2 outline-none"
+              />
+            </div>
+
+            {/* Password */}
+            <label className="block text-black-700 mb-1 font-medium">
+              Password
+            </label>
+            <div className="flex items-center bg-secondary/10 rounded-lg mb-2 p-2 focus-within:ring-2 focus-within:ring-[var(--color-primary)]">
+              <FaLock className="text-black/50 mr-2" />
+              <input
+                type={showPassword ? "text" : "password"}
+                placeholder="Enter your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="w-full p-2 outline-none"
+              />
+            </div>
+
+             <div className="flex items-center justify-between">
+              <div className="flex items-center mb-4">
+                <input
+                  type="checkbox"
+                  checked={showPassword}
+                  onChange={() => setShowPassword(!showPassword)}
+                  className="mr-2"
+                />
+                <label className="text-sm text-gray-600">Show password</label>
+              </div>
+
+              {/* Role toggle */}
+              <div className="flex items-center mb-4">
+                <input
+                  type="checkbox"
+                  checked={role}
+                  onChange={() => setRole(!role)}
+                  className="mr-2"
+                />
+                <label className="text-sm text-gray-600">Register as Admin</label>
+              </div>
+             </div>
+
+            {/* Sign Up button */}
+            <button
+              type="submit"
+              className="px-10 py-3 block mx-auto rounded-lg font-semibold text-white hover:opacity-90 transition mb-4"
+              style={{ backgroundColor: "var(--color-primary)" }}
+            >
+              Sign Up
+            </button>
+          </form>
+
+          <p className="mt-4 text-sm text-gray-600 text-center">
+            Already have an account?{" "}
+            <a
+              href="/login"
+              className="font-medium"
+              style={{ color: "var(--color-primary)" }}
+            >
+              Login
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Signup;
