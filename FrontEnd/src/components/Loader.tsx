0e88d5fc9fd52d7672fb6073463ed33ab4635f68
Brendaline-Yemele <yemeleline2@gmail.com>
2025-09-23 09:57:22 +0100

    updates

---
+++ FrontEnd/src/components/Loader.tsx
@@ -0,0 +1,9 @@
+function Loader() {
+  return (
+    <div className="flex justify-center items-center w-full h-[50vh]">
+      <div className="w-12 h-12 border-4 border-primary border-t-transparent rounded-full animate-spin"></div>
+    </div>
+  );
+}
+
+export default Loader;
