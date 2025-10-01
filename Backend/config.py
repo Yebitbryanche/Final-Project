aa396558b55fe233bca02e94d46a43b488667b61
Yebitbryanche <yebitbryanche@gmail.com>
2025-10-01 19:52:04 +0100

    commit 4

--- Backend/config.py
+++ Backend/config.py
@@ -5,11 +5,11 @@ from pydantic_settings import BaseSettings
 load_dotenv()
 
 class Settings(BaseSettings):
-    SECRET_KEY: str = os.getenv("SECRET_KEY")
-    ACCESS_TOKEN_EXPIRE_MINUTES:int = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
-    ALGORITHM:str = os.getenv("ALGORITHM")
-    DB_URL:str = os.getenv("DB_URL")
-    STRIPE_SECRET_KEY:str = os.getenv("STRIPE_SECRET_KEY")
+    SECRET_KEY: str = "741sdf852re3sdf0sdr7rtjb455s741fgas411f7896214852815rf517416"
+    ACCESS_TOKEN_EXPIRE_MINUTES:int = "30"
+    ALGORITHM:str = "HS256"
+    DB_URL:str = "mysql+pymysql://root@localhost:3306/mboakako"
+    STRIPE_SECRET_KEY:str ="sk_test_51S7yy7LN8y6tjoNtX5VMsILkvcoHvYMa1SIP24FqBoQUfvEZvoIIRIrkpKsg8mPa9zAnGRecN7w5mvqjOADiA9zu00JqCX3Z9r"
 
     class Config:
         env_file = ".env"
