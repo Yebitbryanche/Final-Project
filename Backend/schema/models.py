55177c141775d3ddf02891f1ec77e895da458552
Yebitbryanche <yebitbryanche@gmail.com>
2025-10-02 14:39:33 +0100

    argon 2 for hashing

--- Backend/schema/models.py
+++ Backend/schema/models.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel, EmailStr
 from passlib.context import CryptContext
 from datetime import datetime
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated='auto')
+pwd_context = CryptContext(schemes=["argon2"], deprecated='auto')
 
 
 class User(SQLModel, table=True):
