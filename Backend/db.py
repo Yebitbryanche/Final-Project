37640d83910210a9d3ee8d449b48540a2848f4e1
Yebitbryanche <yebitbryanche@gmail.com>
2025-10-02 17:19:09 +0100

    new db tables

--- Backend/db.py
+++ Backend/db.py
@@ -6,7 +6,9 @@ from config import settings
 engine = create_engine(settings.DB_URL, echo=True)
 
 def create_db_and_tables():
+    SQLModel.metadata.drop_all(engine)
     SQLModel.metadata.create_all(engine)
+
     
 def get_session():
     with Session(engine) as session:
