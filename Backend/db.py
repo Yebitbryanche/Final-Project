c7b33fe36b389d73114ce107981f8d284b680eac
Yebitbryanche <yebitbryanche@gmail.com>
2025-10-02 19:29:54 +0100

    complete deploy

--- Backend/db.py
+++ Backend/db.py
@@ -6,7 +6,6 @@ from config import settings
 engine = create_engine(settings.DB_URL, echo=True)
 
 def create_db_and_tables():
-    SQLModel.metadata.drop_all(engine)
     SQLModel.metadata.create_all(engine)
 
     
