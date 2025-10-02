8588310055fa21082addd92d90ddbc35083b266e
Yebitbryanche <yebitbryanche@gmail.com>
2025-10-02 19:01:30 +0100

    fixing reccomendation issues 2

--- Backend/schema/models.py
+++ Backend/schema/models.py
@@ -31,7 +31,7 @@ class User(SQLModel, table=True):
 class Product(SQLModel, table=True):
     __tablename__ = "Product"
 
-    id:int = Field(primary_key=True, default=None)
+    id: Optional[int] = Field(default=None, primary_key=True)
     title:str = Field(index=True)
     description: str = Field(index=True)
     price: float = Field(index=True)
