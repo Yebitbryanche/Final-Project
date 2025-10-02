25c24b51035fac366bc12d1e71c7dfe73f540f9b
Yebitbryanche <yebitbryanche@gmail.com>
2025-10-02 09:01:51 +0100

    bd url updated

--- Backend/config.py
+++ Backend/config.py
@@ -8,7 +8,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str = "741sdf852re3sdf0sdr7rtjb455s741fgas411f7896214852815rf517416"
     ACCESS_TOKEN_EXPIRE_MINUTES:int = "30"
     ALGORITHM:str = "HS256"
-    DB_URL:str = "mysql+pymysql://root:aOUSfIJyAXmJAEBkOGkGaqpdwgmyhfya@nozomi.proxy.rlwy.net:10905/railway"
+    DB_URL:str =  "mysql+pymysql://root:vZWBfwyNQcJzwUNfbeNfMRUNfexPsGAV@mysql.railway.internal:3306/railway"
     STRIPE_SECRET_KEY:str ="sk_test_51S7yy7LN8y6tjoNtX5VMsILkvcoHvYMa1SIP24FqBoQUfvEZvoIIRIrkpKsg8mPa9zAnGRecN7w5mvqjOADiA9zu00JqCX3Z9r"
 
     class Config:
